5786b5b1be880c2d28d28d740e37abb6ae16c716
David Lau <david.kaimun.lau@gmail.com>
2023-11-18 15:06:57 +0100

    Configure seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Fetching top rated movies..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)["results"]
+
+puts "Creating movies..."
+movies.each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+  puts "Created #{movie["title"]}"
+end
+
+puts "Finished!"
