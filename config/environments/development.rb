f213ddad17d624934fb9727fc87a53e59566b481
David Lau <david.kaimun.lau@gmail.com>
2023-11-19 12:44:15 +0100

    Style index and show.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Avoid running "rake assets:precompile" each time
+  config.assets.prefix = "/dev-assets"
+  config.assets.compile = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
